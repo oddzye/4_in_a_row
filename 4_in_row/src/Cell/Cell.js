93bf3ff099ce46e2fed99746ba3d32c5fe3db532
Dmitry <tzariov.dmitry@yandex.ru>
2019-05-31 00:59:23 +0300

    styles for cell

--- 4_in_row/src/Cell/Cell.js
+++ 4_in_row/src/Cell/Cell.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Cell.css'
+import classNames from 'classnames'
 
 function Cell(props) {
-    return <div className="cell">{props.value}</div>
+    let cellValueClasses = classNames({
+        'cell-value': true,
+        'cell-value__1player': props.value === 1,
+        'cell-value__2player': props.value === 2 
+    })
+    return <div className="cell">
+                <div className={cellValueClasses}>
+                    {props.value}
+                </div> 
+            </div>
 }
 
 export default Cell;
