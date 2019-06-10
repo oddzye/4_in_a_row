d06a11ca43691b4cee41ee62bbcb16f0ca2d6227
Dmitry <tzariov.dmitry@yandex.ru>
2019-06-10 12:42:51 +0300

    start page  init

--- 4_in_row/src/StartPage/StartPage.js
+++ 4_in_row/src/StartPage/StartPage.js
@@ -1,16 +1,41 @@
 import React , {Component} from 'react';
 import { Link } from 'react-router-dom'
+import '../StartPage/StartPage.css'
 
 class StartPage extends Component {
     constructor() {
         super();
+        this.state = {
+            firstPlayerName: null,
+            secondPlayerName: null
+        }
+    }
+
+    onChangeNamePlayer = (e, name) => {
+        switch (name) {
+            case 'firstPlayerName':
+                this.setState({
+                    firstPlayerName: e.target.value
+                });
+                console.log("first player", this.state.firstPlayerName)
+            case 'secondPlayerName':
+            this.setState({
+                secondPlayerName: e.target.value
+            });
+            console.log("second player", this.state.secondPlayerName)
+        }
+        
     }
 
     render() {
+        const firstPlayerName = 'firstPlayerName';
+        const secondPlayerName = 'secondPlayerName';
         return (
             <>
-            <div>Hello</div>
-            <Link to='/game'>Start game!</Link>
+            <div>Введите имена игроков</div>
+            <input onChange={(e, firstPlayerName) => this.onChangeNamePlayer}/>
+            <input onChange={(e, secondPlayerName) => this.onChangeNamePlayer}/>
+                <Link to='/game'>Start game!</Link>
             </>
         )
     }
