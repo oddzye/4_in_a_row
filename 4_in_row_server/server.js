34046832cbcdda4c2964e031d1a3da23db436008
Dmitry <33705300+oddzye@users.noreply.github.com>
2019-06-18 16:27:23 +0300

    router init

--- 4_in_row_server/server.js
+++ 4_in_row_server/server.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/subscribers', {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 
 const db = mongoose.connection
 db.on('error', error => console.log(error));
 db.on('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter)
 
 app.listen(3002, () => console.log("Server started"));
