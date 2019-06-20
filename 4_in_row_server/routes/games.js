de9fc46ffcb0b18b63d08a9cf49ac566331cd505
Dmitry <33705300+oddzye@users.noreply.github.com>
2019-06-20 15:18:05 +0300

    post game

--- 4_in_row_server/routes/games.js
+++ 4_in_row_server/routes/games.js
@@ -18,5 +18,20 @@ router.get('/:id', (req, res) => {
 })
 //Creating ONE
 
+router.post('/', async (req, res) => {
+    try {
+        const game = await new Game(req.body);
+        game.save()
+            .then(game => {
+                res.status(200).json({'game': 'game added succesfully'})
+            })
+            .catch(err => {
+                res.status(400).send('adding new game failed')
+            })
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
 //Updating ONE
 module.exports = router; 
